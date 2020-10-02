4e4d6dc47da6e9877a696469673b4270c4e84cba
sombrero104 <sombrero.khy@gmail.com>
2020-10-02 22:44:02 +0900

    .

--- src/main/java/me/sombrero/byte_buddy/Masulsa.java
+++ src/main/java/me/sombrero/byte_buddy/Masulsa.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
+/**
+ * 바이트코드 조작하기.
+ */
 public class Masulsa {
 
     public static void main(String[] args) {
@@ -41,6 +44,17 @@ public class Masulsa {
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+
+        /**
+         * 방법 3.
+         * Java Agent를 jar 파일로 만들어서 자바 실행 시 아래와 같이 JVM 옵션을 추가하는 방법.
+         *      -javaagent:/Users/sombrero104/IdeaProjects/MasulsaAgent/target/MasulsaAgent-1.0-SNAPSHOT.jar
+         *
+         * => 이 방법은 실행 후 class 파일의 바이트코드를 확인해보면 코드가 변경되지 않은 것을 확인할 수 있다.
+         *      클래스 파일 자체를 변경하는 것이 아니고..
+         *      자바 agent가 클래스 로딩 시점에 적용이 되어 메모리에 올라갈 때 바이트코드가 변경되기 때문..
+         *      (위의 다른 방법들과는 다르게 비침투적인(transparent한) 방식..)
+         */
 
         /**
          * 변경되었는지 출력하는 부분.
